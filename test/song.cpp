0cef6eec2bb1af16a92725369cdb788fc5eabd5c
Narongpol Kijrangsan <28519551+nkd3v@users.noreply.github.com>
2021-11-05 16:50:30 +0700

    Add stopwatch

--- test/song.cpp
+++ test/song.cpp
@@ -1,48 +1,48 @@
-#include "Arduino.h"
+// #include "Arduino.h"
 
-int speakerPin = 8;
+// int speakerPin = 8;
 
-void setup() {
-  pinMode(speakerPin, OUTPUT);
-  Serial.begin(9600);
-}
+// void setup() {
+//   pinMode(speakerPin, OUTPUT);
+//   Serial.begin(9600);
+// }
 
-void spaceGun(int maximum)
-{
-  for (int i=0; i < maximum; i+=2) {
-    digitalWrite(speakerPin, HIGH);
-    delayMicroseconds(i);
-    digitalWrite(speakerPin, LOW);
-    delayMicroseconds(i);    
-  }
-}
+// void spaceGun(int maximum)
+// {
+//   for (int i=0; i < maximum; i+=2) {
+//     digitalWrite(speakerPin, HIGH);
+//     delayMicroseconds(i);
+//     digitalWrite(speakerPin, LOW);
+//     delayMicroseconds(i);    
+//   }
+// }
 
-void randSound(int maximum)
-{
-  tone(speakerPin,random(maximum,10*maximum));
-  delay(maximum);
-}
+// void randSound(int maximum)
+// {
+//   tone(speakerPin,random(maximum,10*maximum));
+//   delay(maximum);
+// }
 
-void fibonacci(int maximum)
-{
-  long fib = 1;
-  long fib1 = 1;  
-  long fib2 = 2;
-  for (int i=0; i < maximum; i++) {
-    fib = fib1+fib2;
-    fib1 = fib2;
-    fib2 = fib;
-    tone(speakerPin,fib);
-    delay(200);
-  }
-  noTone(speakerPin);
-}
+// void fibonacci(int maximum)
+// {
+//   long fib = 1;
+//   long fib1 = 1;  
+//   long fib2 = 2;
+//   for (int i=0; i < maximum; i++) {
+//     fib = fib1+fib2;
+//     fib1 = fib2;
+//     fib2 = fib;
+//     tone(speakerPin,fib);
+//     delay(200);
+//   }
+//   noTone(speakerPin);
+// }
 
-void loop() {
-  spaceGun(1000);
-  delay(5000);  
-  fibonacci(20);
-  delay(5000);  
-  //randSound(50);
-  //delay(5000);
-}
+// void loop() {
+//   spaceGun(1000);
+//   delay(5000);  
+//   fibonacci(20);
+//   delay(5000);  
+//   //randSound(50);
+//   //delay(5000);
+// }
---
+++ test/test2.cpp
@@ -0,0 +1,13 @@
+#include <Arduino.h>
+
+const uint8_t xPin = A0;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  Serial.println(analogRead(xPin));
+}
